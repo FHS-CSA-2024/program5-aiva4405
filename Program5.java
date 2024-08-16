0200d91225e08008411eb528b09b732e2b561868
aiva4405 <58538705+aiva4405@users.noreply.github.com>
2024-08-16 09:13:43 -0700

    Update Program5.java
    completed the requirements

--- Program5.java
+++ Program5.java
@@ -2,8 +2,53 @@
 
 //Your code here
 
+public class Program5 {
+    public static void main(String[] args) {
+        int royaleMiles = 286;
+        int royaleGallons = 9;
+        double royaleMpg = (double)royaleMiles / royaleGallons;
+        
+        int koopaMiles = 412;
+        int koopaGallons = 40;
+        double koopaMpg = (double)koopaMiles / koopaGallons;
+        
+        int pipeMiles = 361;
+        int pipeGallons = 18;
+        double pipeMpg = (double)pipeMiles / pipeGallons;
+        
+        int badMiles = 161;
+        int badGallons = 11; 
+        double badMpg = (double)badMiles / badGallons;
+        
+        System.out.println("Mario kart racer's average miles per gallon:");
+        System.out.print("Royale: ");
+        System.out.format("%.1f", royaleMpg);
+        System.out.println(" mpg");
+        
+        System.out.print("Koopa King: ");
+        System.out.format("%.1f", koopaMpg);
+        System.out.println(" mpg");
+        
+        System.out.print("Pipe Frame: ");
+        System.out.format("%.1f", pipeMpg);
+        System.out.println(" mpg");
+        
+        System.out.print("Badwagon: ");
+        System.out.format("%.1f", badMpg);
+        System.out.println(" mpg");
+        
+    }
+}
+
+
+
 //Paste console output below:
 /*
 
+Mario kart racer's average miles per gallon:
+Royale: 31.8 mpg
+Koopa King: 10.3 mpg
+Pipe Frame: 20.1 mpg
+Badwagon: 14.6 mpg
 
 */
